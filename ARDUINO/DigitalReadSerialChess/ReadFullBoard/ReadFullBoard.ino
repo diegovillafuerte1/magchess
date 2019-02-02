b7f931bd00a8030c264bb5760fb237b9a0d675a0
Diego Villafuerte <diego.villafuerte@hcl.com>
2019-02-01 23:28:34 -0500

    fix syntax, add send to pi empty method

--- ARDUINO/DigitalReadSerialChess/ReadFullBoard/ReadFullBoard.ino
+++ ARDUINO/DigitalReadSerialChess/ReadFullBoard/ReadFullBoard.ino
@@ -1,44 +1,51 @@
-byte rows[] = {2, 3, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36}
+byte rows[] = {2, 3, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
 const int ROWCOUNT = sizeof(rows)/sizeof(rows[0]);
 byte cols[] = {37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53};
 const int COLCOUNT = sizeof(cols)/sizeof(cols[0]);
 
-byte keys[colCount][rowCount];
+byte keys[COLCOUNT][ROWCOUNT];
 
 void setup() {
   Serial.begin(9600);
-  for(int i = 0; i < rowCount; i++) {
+  for(int i = 0; i < ROWCOUNT; i++) {
     Serial.print(rows[i]); Serial.println(" as input");
     pinMode(rows[i], INPUT);
   }
  
-  for (int j = 0; j < colCount; j++) {
+  for (int j = 0; j < COLCOUNT; j++) {
     Serial.print(cols[j]); Serial.println(" as output");
     pinMode(cols[j], OUTPUT);
   }
 }
 
-void writeFunc(colIndex) {
-    for (int writeIndex = 0; writeIndex < COLCOUNT; writeIndex++) {
+void writeFunc(int colIndex) {
+  for (int writeIndex = 0; writeIndex < COLCOUNT; writeIndex++) {
     if (colIndex == writeIndex ) {
-      digitalWrite(cols[writeIndex], HIGH) 
+      digitalWrite(cols[writeIndex], HIGH) ;
     }
     else {
-      digitalWrite(cols[writeIndex], LOW)
+      digitalWrite(cols[writeIndex], LOW);
     }
   }
 }
 
-void readFunc() {
-    for (int rowIndex = 0; rowIndex < ROWCOUNT; rowIndex++) {
-    digitalRead(rows[rowIndex]);
+void readFunc(int out[16]) {
+  for (int rowIndex = 0; rowIndex < ROWCOUNT; rowIndex++) {
+    int pinState = digitalRead(rows[rowIndex]);
+    out[rowIndex] = pinState;
   }
+}
+
+void sendRowToPi(int pinCheck[]) {
+  //send to pi
 }
 
 void writeAndRead() {
   for (int colIndex = 0; colIndex < COLCOUNT; colIndex++) {
     writeFunc(colIndex);
-    readFunc();
+    int readOut[16];
+    readFunc(readOut);
+    sendRowToPi(readOut);
   }
 }
 void loop() {
